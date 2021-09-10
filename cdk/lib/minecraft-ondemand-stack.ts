ba22d105333c1f9ef9d6fdaaa4b4ece3cc7d5c1b
Louis <xcalllibur@outlook.com>
2021-09-11 04:06:46 +0800

    Added code to create Fargate resources

--- cdk/lib/minecraft-ondemand-stack.ts
+++ cdk/lib/minecraft-ondemand-stack.ts
@@ -1,10 +1,18 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as efs from '@aws-cdk/aws-efs';
+import * as ecs from '@aws-cdk/aws-ecs';
+import { NetworkLoadBalancedFargateService } from '@aws-cdk/aws-ecs-patterns';
 
 export interface MinecraftStackProps extends cdk.StackProps
 {
     vpcId: string;
+}
+
+interface FileSystemDetails
+{
+    fileSystemId: string;
+    accessPointId: string;
 }
 
 export class MinecraftStack extends cdk.Stack
@@ -15,10 +23,12 @@ export class MinecraftStack extends cdk.Stack
 
         const vpc = ec2.Vpc.fromLookup(this, "MinecraftVpc", { vpcId: props.vpcId });
 
-        const fileSystemAccessPointArn = this.createFileSystem(vpc);
+        const fileSystemDetails = this.createFileSystem(vpc);
+
+        this.createEcs(vpc, fileSystemDetails);
     }
 
-    private createFileSystem(vpc: ec2.IVpc): string
+    private createFileSystem(vpc: ec2.IVpc): FileSystemDetails
     {
         const fileSystem = new efs.FileSystem(this, 'MinecraftFileSystem', {
             vpc: vpc
@@ -37,6 +47,84 @@ export class MinecraftStack extends cdk.Stack
             }
         });
 
-        return accessPoint.accessPointArn;
+        return {
+            fileSystemId: fileSystem.fileSystemId,
+            accessPointId: accessPoint.accessPointId
+        };
+    }
+
+    private createEcs(vpc: ec2.IVpc, fileSystemDetails: FileSystemDetails)
+    {
+        const serviceName = "minecraft-server";
+
+        var service = new NetworkLoadBalancedFargateService(this, "MinecraftService", {
+            serviceName: serviceName,
+            vpc: vpc,
+            platformVersion: ecs.FargatePlatformVersion.VERSION1_4,
+            taskImageOptions: {
+                containerName: "minecraft-ecsfargate-watchdog",
+                image: ecs.ContainerImage.fromRegistry("doctorray/minecraft-ecsfargate-watchdog"),
+                environment: {
+                    "CLUSTER": "minecraft",
+                    "SERVICE": serviceName,
+                    "DNSZONE": "TODO",
+                    "SERVERNAME": "TODO",
+                    "SNSTOPIC": "TODO",
+                    "TWILIOFROM": "TODO",
+                    "TWILIOTO": "TODO",
+                    "TWILIOAID": "TODO",
+                    "TWILIOAUTH": "TODO",
+                    "STARTUPMIN": "10",
+                    "SHUTDOWNMIN": "20"
+                }
+            },
+            memoryLimitMiB: 2048,
+            cpu: 1024
+        });
+
+        const dataVolumeName = "MinecraftDataVolume";
+
+        service.taskDefinition.addVolume({
+            name: dataVolumeName,
+            efsVolumeConfiguration: {
+                fileSystemId: fileSystemDetails.fileSystemId,
+                authorizationConfig: {
+                    accessPointId: fileSystemDetails.accessPointId
+                },
+                rootDirectory: "data"
+            }
+        });
+
+        var serverContainer = service.taskDefinition.addContainer("MinecraftServerContainer", {
+            containerName: "minecraft-server",
+            image: ecs.ContainerImage.fromRegistry("itzg/minecraft-server"),
+            portMappings: [
+                { containerPort: 25565, hostPort: 25565 }
+            ],
+            environment: {
+                "EULA": "TRUE"
+            },
+        });
+
+        serverContainer.addMountPoints({
+            sourceVolume: dataVolumeName,
+            readOnly: false,
+            containerPath: "/data",
+        });
+
+        // Escape hatch to set launch type to FARGATE_SPOT for cheaper run costs
+        // const cfnService = service.node.tryFindChild('Service') as ecs.CfnService
+
+        // cfnService.launchType = undefined
+        // cfnService.capacityProviderStrategy = [
+        //     {
+        //         capacityProvider: 'FARGATE_SPOT',
+        //         weight: 4,
+        //     },
+        //     {
+        //         capacityProvider: 'FARGATE',
+        //         weight: 1,
+        //     },
+        // ]
     }
 }
