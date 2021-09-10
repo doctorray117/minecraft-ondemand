f384bccd1c8f2ffbc32a2ed87090144475f5f8b3
Louis <xcalllibur@outlook.com>
2021-09-11 02:17:29 +0800

    Added code to create EFS resources

--- cdk/bin/minecraft-ondemand.ts
+++ cdk/bin/minecraft-ondemand.ts
@@ -6,19 +6,19 @@ import { MinecraftStack } from '../lib/minecraft-ondemand-stack';
 const app = new cdk.App();
 
 new MinecraftStack(app, 'MinecraftStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+    /* If you don't specify 'env', this stack will be environment-agnostic.
+     * Account/Region-dependent features and context lookups will not work,
+     * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+    /* Uncomment the next line to specialize this stack for the AWS Account
+     * and Region that are implied by the current CLI configuration. */
+    // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  env: { account: "INSERT", region: "INSERT" },
+    /* Uncomment the next line if you know exactly what Account and Region you
+     * want to deploy the stack to. */
+    env: { account: "INSERT", region: "INSERT" },
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+    /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 
-  vpcId: "INSERT"
+    vpcId: "INSERT"
 });
--- cdk/lib/minecraft-ondemand-stack.ts
+++ cdk/lib/minecraft-ondemand-stack.ts
@@ -1,17 +1,42 @@
-import { Vpc } from '@aws-cdk/aws-ec2';
-import { Construct, Stack, StackProps } from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as efs from '@aws-cdk/aws-efs';
 
-export interface MinecraftStackProps extends StackProps
+export interface MinecraftStackProps extends cdk.StackProps
 {
     vpcId: string;
 }
 
-export class MinecraftStack extends Stack
+export class MinecraftStack extends cdk.Stack
 {
-    constructor(scope: Construct, id: string, props: MinecraftStackProps)
+    constructor(scope: cdk.Construct, id: string, props: MinecraftStackProps)
     {
         super(scope, id, props);
 
-        const vpc = Vpc.fromLookup(this, "MinecraftVpc", { vpcId: props.vpcId });
+        const vpc = ec2.Vpc.fromLookup(this, "MinecraftVpc", { vpcId: props.vpcId });
+
+        const fileSystemAccessPointArn = this.createFileSystem(vpc);
+    }
+
+    private createFileSystem(vpc: ec2.IVpc): string
+    {
+        const fileSystem = new efs.FileSystem(this, 'MinecraftFileSystem', {
+            vpc: vpc
+        });
+
+        const accessPoint = fileSystem.addAccessPoint("MinecraftAccessPoint", {
+            path: "/minecraft",
+            posixUser: {
+                uid: "1000",
+                gid: "1000"
+            },
+            createAcl: {
+                ownerUid: "1000",
+                ownerGid: "1000",
+                permissions: "0755"
+            }
+        });
+
+        return accessPoint.accessPointArn;
     }
 }
