b02351dc2891b2dff717ef4425a6350e894045b0
Louis <xcalllibur@outlook.com>
2021-09-11 01:41:13 +0800

    Created skeleton CDK application

---
+++ cdk/lib/minecraft-ondemand-stack.ts
@@ -0,0 +1,11 @@
+import * as cdk from '@aws-cdk/core';
+
+export class MinecraftStack extends cdk.Stack
+{
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps)
+  {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+  }
+}
