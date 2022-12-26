eb8182a3c1dd3db48e3d24f44517f49664189424
Greg Krause <gregkrause@gregs-mbp.lan>
2022-12-25 23:18:41 -0600

    Revert "fix: always pull watchdog from registry (prevent arm64 build conflicts)"
    This reverts commit 2911e3a8868615cdd4261b4985313669cfb5fe83.

--- cdk/lib/minecraft-stack.ts
+++ cdk/lib/minecraft-stack.ts
@@ -223,9 +223,13 @@ export class MinecraftStack extends Stack {
       'WatchDogContainer',
       {
         containerName: constants.WATCHDOG_SERVER_CONTAINER_NAME,
-        image: ecs.ContainerImage.fromRegistry(
-            'doctorray/minecraft-ecsfargate-watchdog'
-        ),
+        image: isDockerInstalled()
+          ? ecs.ContainerImage.fromAsset(
+              path.resolve(__dirname, '../../minecraft-ecsfargate-watchdog/')
+            )
+          : ecs.ContainerImage.fromRegistry(
+              'doctorray/minecraft-ecsfargate-watchdog'
+            ),
         essential: true,
         taskDefinition: taskDefinition,
         environment: {
